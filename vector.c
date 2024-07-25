1e845509597ad67c4bd5314e7a1cdd8b096ab0eb
Diego Cabrera <90268530+Diegcor@users.noreply.github.com>
2024-07-25 12:13:47 -0600

    created helper functions and reworked vector add method

--- vector.c
+++ vector.c
@@ -51,6 +51,110 @@ void LAL_vector_null(Vector* v)
 		}
 }
 
+/*
+Allocates an array which is returned
+*/
+int* LAL_field_to_ints_helper(VecField f, VecType t, size_t size)
+{
+	int* is = (int*)malloc(size * sizeof(int));
+
+	if (VecType == FLOAT)
+	{
+		float* flts = f.floats;
+		for (int i = 0; i < size; i++)
+		{
+			is[i] = (int)flts[i];
+		}
+	}
+	else if (VecType == DOUBLE)
+	{
+		double* dbls = f.doubles;
+		for (int i = 0; i < size; i++)
+		{
+			is[i] = (double)dbls[i];
+		}
+	}
+	else
+	{
+		int* ins = f.ints;
+		for (int i = 0; i < size; i++)
+		{
+			is[i] = ins[i];
+		}
+	}
+
+	return is;
+}
+
+/*
+Allocates an array which is returned
+*/
+float* LAL_field_to_floats_helper(VecField f, VecType t, size_t size)
+{
+	float* fs = (float*)malloc(size * sizeof(float));
+
+	if (VecType == INT)
+	{
+		int* ins = f.ints;
+		for (int i = 0; i < size; i++)
+		{
+			fs[i] = (float)ins[i];
+		}
+	}
+	else if (VecType == DOUBLE)
+	{
+		double* dbls = f.doubles;
+		for (int i = 0; i < size; i++)
+		{
+			fs[i] = (float)dbls[i];
+		}
+	}
+	else
+	{
+		float* flts = f.floats;
+		for (int i = 0; i < size; i++)
+		{
+			fs[i] = flts[i];
+		}
+	}
+
+	return fs;
+}
+
+/*
+Allocates an array which is returned
+*/
+double* LAL_field_to_doubles_helper(VecField f, VecType t, size_t size)
+{
+	double* ds = (double*)malloc(size * sizeof(double));
+
+	if (VecType == INT)
+	{
+		int* ins = f.ints;
+		for (int i = 0; i < size; i++)
+		{
+			ds[i] = (double)ins[i];
+		}
+	}
+	else if (VecType == FLOAT)
+	{
+		double* flts = f.floats;
+		for (int i = 0; i < size; i++)
+		{
+			ds[i] = (double)flts[i];
+		}
+	}
+	else
+	{
+		double* dbls = f.doubles;
+		for (int i = 0; i < size; i++)
+		{
+			ds[i] = (float)dbls[i];
+		}
+	}
+
+	return ds;
+}
 
 int LAL_vector_add(Vector* v1, Vector* v2, Vector* res)
 {
@@ -65,90 +169,47 @@ int LAL_vector_add(Vector* v1, Vector* v2, Vector* res)
 		LAL_vector_init(res, most_precise, smallest);
 	}
 
-	int int1, int2;
-	int flt1, flt2;
-	int dbl1, dbl2;
-	for (int i = 0; i < smallest; i++)
+	if (res->type == INT)
 	{
-		if (res->type == INT)
+		int* ints1, ints2, resInts;
+
+		ints1 = LAL_field_to_ints_helper(v1->field, v1->type, smallest);
+		ints2 = LAL_field_to_ints_helper(v2->field, v2->type, smallest);
+		resInts = res->field.ints;
+
+		for (int i = 0; i < smallest; i++)
 		{
-			if (v1->type == FLOAT)
-			{
-				int1 = (int) v1->field.floats[i];
-			}
-			else if (v1->type == DOUBLE)
-			{
-				int1 = (int) v1->field.doubles[i];
-			}
-			else
-				int1 = v1->field.ints[i];
-
-			if (v2->type == FLOAT)
-			{
-				int2 = (int) v2->field.floats[i];
-			}
-			else if (v2->type == DOUBLE)
-			{
-				int2 = (int) v2->field.doubles[i];
-			}
-			else
-				int2 = v2->field.ints[i];
-
-			res->field.ints[i] = int1 + int2;
-		}
-		else if (res->type == FLOAT)
-		{
-			if (v1->type == INT)
-			{
-				flt1 = (float)v1->field.ints[i];
-			}
-			else if (v1->type == DOUBLE)
-			{
-				flt1 = (float) v1->field.doubles[i];
-			}
-			else
-				flt1 = v1->field.floats[i];
-
-			if (v2->type == INT)
-			{
-				flt2 = (float)v2->field.ints[i];
-			}
-			else if (v2->type == DOUBLE)
-			{
-				flt2 = (float)v2->field.doubles[i];
-			}
-			else
-				flt2 = v2->field.floats[i];
-
-			res->field.ints[i] = int1 + int2;
-		}
-		else if (res->type == DOUBLE)
-		{
-			if (v1->type == INT)
-			{
-				dbl1 = (double)v1->field.ints[i];
-			}
-			else if (v1->type == FLOAT)
-			{
-				dbl1 = (double)v1->field.floats[i];
-			}
-			else
-				dbl1 = v1->field.doubles[i];
-
-			if (v2->type == INT)
-			{
-				dbl2 = (double)v2->field.ints[i];
-			}
-			else if (v2->type == FLOAT)
-			{
-				dbl2 = (double)v2->field.floats[i];
-			}
-			else
-				dbl2 = v2->field.doubles[i];
-
-			res->field.ints[i] = int1 + int2;
+			resInts[i] = ints1[i] + ints2[i];
 		}
 	}
+	else if (res->type == FLOAT)
+	{
+		float* flts1, flts2, resflts;
+
+		flts1 = LAL_field_to_floats_helper(v1->field, v1->type, smallest);
+		flts2 = LAL_field_to_floats_helper(v2->field, v2->type, smallest);
+		resflts = res->field.floats;
+
+		for (int i = 0; i < smallest; i++)
+		{
+			resflts[i] = flts1[i] + flts2[i];
+		}
+	}
+	else
+	{
+		double* dbls1, dbls2, resdbls;
+
+		dbls1 = LAL_field_to_doubles_helper(v1->field, v1->type, smallest);
+		dbls2 = LAL_field_to_doubles_helper(v2->field, v2->type, smallest);
+		resdbls = res->field.dbls;
+
+		for (int i = 0; i < smallest; i++)
+		{
+			resdbls[i] = dbls1[i] + dbls2[i];
+		}
+	}
+
+	return 0;
 }
 
 int LAL_vector_subtract(Vector* v1, Vector* v2, Vector* res){}
